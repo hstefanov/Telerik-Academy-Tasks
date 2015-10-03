787e98af31e55ecc77fea95ad8e2bc836db032dc
hstefanov <stefanov.h.s@gmail.com>
2015-10-03 22:15:21 +0300

    final

---
+++ 3. Software Technologies/1. Databases/02. Processing XML in .NET/ExtractAlbums/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace ExtractAlbums
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Xml;
+
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (XmlTextWriter writer = new XmlTextWriter("../../../albums.xml", Encoding.UTF8))
+                {
+                    writer.Formatting = Formatting.Indented;
+                    writer.IndentChar = ' ';
+                    writer.Indentation = 2;
+
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("albums");
+
+                    using (XmlReader reader = XmlReader.Create("../../../catalogue.xml"))
+                    {
+                        while (reader.Read())
+                        {
+                            if (reader.NodeType == XmlNodeType.Element)
+                            {
+                                if (reader.Name == "name")
+                                {
+                                    writer.WriteStartElement("album");
+                                    writer.WriteElementString("name", reader.ReadElementContentAsString());
+                                }
+                                else if (reader.Name == "artist")
+                                {
+                                    writer.WriteElementString("artists",reader.ReadElementContentAsString());
+                                    writer.WriteEndElement();
+                                }
+                            }
+                        }
+                    }
+
+                    writer.WriteEndDocument();
+                    Console.WriteLine("albums saved as albums.xml");
+                } 
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ 3. Software Technologies/1. Databases/02. Processing XML in .NET/XslTransform/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace XslTransform
+{
+    using System;
+    using System.Xml.Xsl;
+    using System.IO;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                XslCompiledTransform xslt = new XslCompiledTransform();
+                xslt.Load("../../../style.xslt");
+                xslt.Transform("../../../catalogue.xml", "../../../catalogue.html");
+                Console.WriteLine("result saved as catalogue.html");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
